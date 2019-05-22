485fb4fa76bad717d023bee997675c1fba2b0c3e
Ridwan Surya Putra <ridwanputra72@gmail.com>
2019-05-23 03:58:03 +0700

    Initial Commit

---
+++ Mujigae Selector/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mujigae Selector
+//
+//  Created by Anterin on 23/05/19.
+//  Copyright © 2019 Ridwan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
